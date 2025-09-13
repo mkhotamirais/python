942167d44892c164206ad0b297087a7fde5c2258
mkhotamirais <mkhotamirais@gmail.com>
2025-09-13 08:40:40 +0700

    update less1

--- less1.py
+++ less1.py
@@ -1,1 +1,13 @@
-print('hello world')
+# Python bahasa pemrograman tingkat tinggi yang mudah dibaca. Oleh Guido van Rossum, rilis 1991. Untuk web development (server-side), software development, mathematics, system scripting.
+# Kebanyakan PC ddan MAC sudah default terinstall python. cek di terminal "python --version"
+# pertama-tama buat file hello.py isinya "print('hello world')". buka terminal, arahkan path ke file itu dan ketik "python hello.py"
+# jika ingin coba di terminal ketik saja "python" atau "py" di terminal, lalu ketik syntax python.
+## Aturan Variabel
+# Variabel harus dimulai dengan huruf atau underscore (_) tidak boleh dimulai angka, hanya mengandung alphanumeric A-z0-9 dan underscore.
+# Variabel itu case-sensitive dan tidak boleh python keyword berikut: and, as, assert, break, class, continue, def, del, elif, else, except, false, finally, for, from, global, if, import, in, is, lambda, None, nonlocal, not, or, pass, raise, return, true, try, while, with, yield
+# Variabel bisa berupa: camelCase: myVariable, PascalCase: MyVariable, camel_case: my_variable.
+
+x = "ahmad"
+y = z = "adul"
+a, b, c = "satu", "dua", "tiga"
+
